3723e66f4afadaa14aaff9ca3f3fcf25791f4d78
김용해 <yoho1019@gmail.com>
2025-05-01 00:36:55 +0900

    다음 페이지 test용 제작

--- DonGasReview/pages/NextView.swift
+++ DonGasReview/pages/NextView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct NextView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Next View Page!!!")
     }
 }
 
