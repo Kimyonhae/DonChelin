43568c9858e065f26152192e2fc591afc440d710
김용해 <yoho1019@gmail.com>
2025-04-30 21:59:10 +0900

    돈슐랭 : init

---
+++ DonGasReview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DonGasReview
+//
+//  Created by 김용해 on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DonGasReview/DonGasReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonGasReviewApp.swift
+//  DonGasReview
+//
+//  Created by 김용해 on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DonGasReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
