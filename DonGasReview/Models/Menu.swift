f5eaeb9c43b84a18753738c5896b5b8259a51d9a
김용해 <yoho1019@gmail.com>
2025-05-01 00:36:28 +0900

    홈 메뉴 모델 생성

---
+++ DonGasReview/Models/Menu.swift
@@ -0,0 +1,13 @@
+//
+//  Menu.swift
+//  DonGasReview
+//
+//  Created by 김용해 on 5/1/25.
+//
+import SwiftUI
+
+struct Menu: Identifiable{
+    let id: UUID = UUID()
+    let title: String
+    let isDestination: Bool = false
+}
