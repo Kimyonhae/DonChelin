7b8f80e853eec73d4dbf46b2abddd769621516f5
김용해 <yoho1019@gmail.com>
2025-05-01 23:51:14 +0900

    돈가스 투어 상세 뷰 생성

---
+++ DonGasReview/pages/DonTourView.swift
@@ -0,0 +1,18 @@
+//
+//  DonTourView.swift
+//  DonGasReview
+//
+//  Created by 김용해 on 5/1/25.
+//
+
+import SwiftUI
+
+struct DonTourView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DonTourView()
+}
