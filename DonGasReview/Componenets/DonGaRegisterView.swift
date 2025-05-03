687c22ab2ec0de64a1489228e090dc304075f19b
김용해 <yoho1019@gmail.com>
2025-05-03 18:07:14 +0900

    앱 로고 적용

---
+++ DonGasReview/Componenets/DonGaRegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  DonGaRegisterView.swift
+//  DonGasReview
+//
+//  Created by 김용해 on 5/3/25.
+//
+
+import SwiftUI
+
+struct DonGaRegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DonGaRegisterView()
+}
