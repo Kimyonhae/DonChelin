c3811c84e53223ac3bbd40348d12dc456ab51d2c
김용해 <yoho1019@gmail.com>
2025-05-03 18:08:31 +0900

    돈가스 리뷰 등록 페이지 생성

--- DonGasReview/Componenets/DonGaRegisterView.swift
+++ DonGasReview/Componenets/DonGaRegisterView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct DonGaRegisterView: View {
+    @Binding var storeName: String
+    @Binding var address: String
+    @Binding var note: String
+    @State private var stars: Int = 0
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading) {
+                customTextField(placeholder: "가게 이름", text: $storeName)
+                HStack {
+                    Image(systemName: "location")
+                    Text("여기에는 이제 주소가 들어갈 예정입니다")
+                }
+                .padding(.vertical, 10)
+                HStack {
+                    ForEach(0..<5, id: \.self) { index in
+                        Image(systemName: index <= stars ? "star.fill" : "star")
+                            .foregroundStyle(index <= stars ? .yellow : .black)
+                            .onTapGesture {
+                                stars = index
+                            }
+                    }
+                }
+                .padding(.vertical ,10)
+                
+                TextEditor(text: $note)
+                    .frame(height: 150)
+                    .padding()
+                    .scrollContentBackground(.hidden)
+                    .background(Color(hex: "F6F8FE"))
+                    .clipShape(.rect(cornerRadius: 8))
+
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("등록하기")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                })
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+    
+    private func customTextField(placeholder: String, text: Binding<String>) -> some View {
+        TextField(text: text) {
+            Text(placeholder)
+        }
+        .padding()
+        .background(Color(hex: "F6F8FE"))
+        .clipShape(.rect(cornerRadius: 8))
     }
 }
 
 #Preview {
-    DonGaRegisterView()
+    DonGaRegisterView(storeName: .constant(""), address: .constant(""), note: .constant(""))
 }
