25fbff9c04da47c4c76f051b40b9ca4e78a26029
김용해 <yoho1019@gmail.com>
2025-05-03 18:07:43 +0900

    swiftData 영속성 추가

--- DonGasReview/DonGasReviewApp.swift
+++ DonGasReview/DonGasReviewApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct DonGasReviewApp: App {
@@ -13,5 +14,6 @@ struct DonGasReviewApp: App {
         WindowGroup {
             HomeView()
         }
+        .modelContainer(for: Review.self)
     }
 }
