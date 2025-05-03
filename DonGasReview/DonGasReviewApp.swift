dabeed908904edebd92875c86a68eb2b7a802a36
김용해 <yoho1019@gmail.com>
2025-05-03 19:13:10 +0900

    modelContainer 생성

--- DonGasReview/DonGasReviewApp.swift
+++ DonGasReview/DonGasReviewApp.swift
@@ -10,10 +10,23 @@ import SwiftData
 
 @main
 struct DonGasReviewApp: App {
+    var modelContainer: ModelContainer = {
+        let schema = Schema([
+            Review.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema,isStoredInMemoryOnly: false)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: modelConfiguration)
+        } catch {
+            fatalError("ModelContainer를 생성 할 수 없습니다")
+        }
+    }()
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
-        .modelContainer(for: Review.self)
+        .modelContainer(modelContainer)
     }
 }
