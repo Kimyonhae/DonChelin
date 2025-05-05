73445755f50d2c8acf547d671d8cfbc15861a222
김용해 <yoho1019@gmail.com>
2025-05-05 19:06:19 +0900

    KaKao API 위도 , 경도 로 좌표 가져오는 컴바인 구독 함수

---
+++ DonGasReview/ViewModel/DongaRegisterViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  DongaRegisterViewModel.swift
+//  DonGasReview
+//
+//  Created by 김용해 on 5/5/25.
+//
+
+import Combine
+import Foundation
+
+class DongaRegisterViewModel: ObservableObject {
+    @Published var storeName: String = ""
+    @Published var address: String = ""
+    @Published var note: String = ""
+    @Published var isLoading: Bool = false
+    let latitude: Double
+    let longitude: Double
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+        print(latitude)
+        print(longitude)
+        // addressCheck()
+    }
+    
+    private func addressCheck() {
+        guard let url = URL(string: "https://dapi.kakao.com/v2/local/geo/coord2address.json?x=\(longitude)&y=\(latitude)&input_coord=WGS84") else {
+            return
+        }
+        self.isLoading = true
+        var request = URLRequest(url: url)
+        request.setValue("KakaoAK 일단 뺌", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { result -> Data in
+                guard let response = result.response as? HTTPURLResponse, response.statusCode == 200  else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                return result.data
+            }
+            .decode(type: AddressResponse.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let err):
+                    print("Address Errorr Message : \(err.localizedDescription)")
+                case .finished:
+                    self.isLoading = false
+                }
+            }, receiveValue: { [weak self] value in
+                if let existValue = value.documents.first {
+                    self?.address = existValue.address.address_name
+                    print("address result : \(self?.address)")
+                }else {
+                    self?.isLoading = true
+                }
+            })
+            .store(in: &cancellables)
+    }
+}
+
+
+
+
+
+
+
+
+
+
